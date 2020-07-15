d37cb6fc19c309b26d02f3cc7321755886d21fd4
Uduak Essien <acushla4real@gmail.com>
2020-07-15 11:37:18 +0100

    File reader function added to check error class

---
+++ lib/checks.rb
@@ -0,0 +1,21 @@
+require 'colorize'
+
+class CheckError
+  attr_reader :err_msg, :file_lines
+  def initialize(file_path)
+    @err_msg = ''
+
+    begin
+      @file_lines = File.readlines(file_path)
+      @file_lines_count = @file_lines.size
+    rescue StandardError => e
+      @file_lines = []
+      @err_msg = "Check file name or path again\n".colorize(:light_red) + e.to_s.colorize(:red)
+    end
+
+  end
+end
+
+check = CheckError.new('records.txt')
+puts check.err_msg
+pp check.file_lines
