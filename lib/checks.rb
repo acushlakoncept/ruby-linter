d2380e872bfebc0b6d9ad32e6cef642f2dd1e4a5
Uduak Essien <acushla4real@gmail.com>
2020-07-15 11:45:24 +0100

    refactor checks.rb: move reader method to a class of it's own

--- lib/checks.rb
+++ lib/checks.rb
@@ -1,21 +1,21 @@
-require 'colorize'
+# require 'colorize'
 
-class CheckError
-  attr_reader :err_msg, :file_lines, :file_lines_count
-  def initialize(file_path)
-    @err_msg = ''
+# class CheckError
+#   attr_reader :err_msg, :file_lines, :file_lines_count
+#   def initialize(file_path)
+#     @err_msg = ''
 
-    begin
-      @file_lines = File.readlines(file_path)
-      @file_lines_count = @file_lines.size
-    rescue StandardError => e
-      @file_lines = []
-      @err_msg = "Check file name or path again\n".colorize(:light_red) + e.to_s.colorize(:red)
-    end
+#     begin
+#       @file_lines = File.readlines(file_path)
+#       @file_lines_count = @file_lines.size
+#     rescue StandardError => e
+#       @file_lines = []
+#       @err_msg = "Check file name or path again\n".colorize(:light_red) + e.to_s.colorize(:red)
+#     end
 
-  end
-end
+#   end
+# end
 
-check = CheckError.new('records.txt')
-puts check.err_msg
-pp check.file_lines
+# check = CheckError.new('records.txt')
+# puts check.err_msg
+# pp check.file_lines
---
+++ lib/file_reader.rb
@@ -0,0 +1,17 @@
+require 'colorize'
+
+class FileReader
+  attr_reader :err_msg, :file_lines, :file_lines_count
+  def initialize(file_path)
+    @err_msg = ''
+
+    begin
+      @file_lines = File.readlines(file_path)
+      @file_lines_count = @file_lines.size
+    rescue StandardError => e
+      @file_lines = []
+      @err_msg = "Check file name or path again\n".colorize(:light_red) + e.to_s.colorize(:red)
+    end
+
+  end
+end
